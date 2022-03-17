6c186a0877b93ee995562a1504fc60ad73e5a6c7
behnam1369 <behnam.aghaali@yahoo.com>
2022-03-17 15:26:37 +0330

    Add logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import './caclulator.css';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+    };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  handleButtonClick(char) {
+    this.setState((state) => (calculate(state, char)));
+  }
+
+  setDisplay(content) {
+    this.setState(() => ({ next: content }));
+  }
+
   render() {
+    const { next, total } = this.state;
     return (
       <div className="calculator">
-        <input type="number" />
-        <button type="button" className="silver">AC</button>
-        <button type="button" className="silver">+/-</button>
-        <button type="button" className="silver">%</button>
-        <button type="button" className="orange">÷</button>
-        <button type="button" className="silver">7</button>
-        <button type="button" className="silver">8</button>
-        <button type="button" className="silver">9</button>
-        <button type="button" className="orange">x</button>
-        <button type="button" className="silver">4</button>
-        <button type="button" className="silver">5</button>
-        <button type="button" className="silver">6</button>
-        <button type="button" className="orange">-</button>
-        <button type="button" className="silver">1</button>
-        <button type="button" className="silver">2</button>
-        <button type="button" className="silver">3</button>
-        <button type="button" className="orange">+</button>
-        <button type="button" className="silver btn0">0</button>
-        <button type="button" className="silver">.</button>
-        <button type="button" className="orange">=</button>
+        <input type="text" value={next || total || ''} onChange={(e) => this.setDisplay(e.target.value)} />
+        <button type="button" className="silver" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>AC</button>
+        <button type="button" className="silver" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>+/-</button>
+        <button type="button" className="silver" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>%</button>
+        <button type="button" className="orange" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>÷</button>
+        <button type="button" className="silver" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>7</button>
+        <button type="button" className="silver" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>8</button>
+        <button type="button" className="silver" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>9</button>
+        <button type="button" className="orange" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>x</button>
+        <button type="button" className="silver" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>4</button>
+        <button type="button" className="silver" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>5</button>
+        <button type="button" className="silver" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>6</button>
+        <button type="button" className="orange" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>-</button>
+        <button type="button" className="silver" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>1</button>
+        <button type="button" className="silver" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>2</button>
+        <button type="button" className="silver" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>3</button>
+        <button type="button" className="orange" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>+</button>
+        <button type="button" className="silver btn0" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>0</button>
+        <button type="button" className="silver" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>.</button>
+        <button type="button" className="orange" onClick={(e) => this.handleButtonClick(e.target.innerHTML)}>=</button>
       </div>
     );
   }
