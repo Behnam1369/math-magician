46bc1d494f1e4a36ad3c6c7ae49c69d67a1548b7
behnam1369 <behnam.aghaali@yahoo.com>
2022-03-23 20:02:22 +0430

    Modify router links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Header from './components/Header';
 import Calculator from './components/Calculator';
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ const Header = () => (
     <h1>Math Magician</h1>
     <nav>
       <ul>
-        <li><a href="./">Home</a></li>
-        <li><a href="./Calculator">Calculator</a></li>
-        <li><a href="./Quote">Quote</a></li>
+        <li><a href="#/">Home</a></li>
+        <li><a href="#/Calculator">Calculator</a></li>
+        <li><a href="#/Quote">Quote</a></li>
       </ul>
     </nav>
   </header>
